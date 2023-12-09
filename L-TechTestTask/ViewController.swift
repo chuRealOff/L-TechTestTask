82310ef22f48b0c0719faac5da6149e3adbb12a9
Dmitry Churilov <98746743+chuRealOff@users.noreply.github.com>
2023-12-09 22:42:54 +0300

    Initial Commit

---
+++ L-TechTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  L-TechTestTask
+//
+//  Created by CHURILOV DMITRIY on 09.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
