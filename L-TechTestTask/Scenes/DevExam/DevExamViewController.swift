ce56f9571f6fba6c59250288d6583465ec421a1f
Dmitry Churilov <98746743+chuRealOff@users.noreply.github.com>
2023-12-15 01:01:26 +0300

    ui: include tableView property
    - UITableViewDelegate behavior implemented
    - UITableViewDataSource behavior implemented

--- L-TechTestTask/Scenes/DevExam/DevExamViewController.swift
+++ L-TechTestTask/Scenes/DevExam/DevExamViewController.swift
@@ -18,22 +18,78 @@ final class DevExamViewController: UITabBarController {
 	// MARK: - Private Properties
 	private var newsData: [NewsModel] = []
 	private let interactor: IDevExamInteractor
-	private var tableView = UITableView()
+	lazy private var tableView: UITableView = makeTableView()
 
 	// MARK: - Initializers
 	init(interactor: IDevExamInteractor) {
 		self.interactor = interactor
 		super.init(nibName: nil, bundle: nil)
 	}
-	
+
 	required init?(coder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
 	}
-	
+
 	// MARK: - ViewController Lifecycle
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		interactor.viewIsReady()
+		view.backgroundColor = .white
+		title = "Лента новостей"
+	}
+
+	override func viewWillLayoutSubviews() {
+		setupUI()
+	}
+
+	// MARK: - Private Methods
+	private func tabBarControllerSetup() {
+
+	}
+	
+	/// Создаёт таблицу для  свойства своего класса.
+	private func makeTableView() -> UITableView {
+		let tableView = UITableView()
+		tableView.register(DevExamTableViewCell.self, forCellReuseIdentifier: DevExamTableViewCell.identifier)
+		tableView.backgroundView?.backgroundColor = .yellow
+		tableView.allowsSelection = true
+		tableView.delegate = self
+		tableView.dataSource = self
+
+		return tableView
+	}
+
+	/// Настраивает размещение всех дочерних view элементов на экране.
+	private func setupUI() {
+		view.addSubview(tableView)
+//		tableView.translatesAutoresizingMaskIntoConstraints = false
+
+		tableView.snp.makeConstraints { make in
+			make.top.equalToSuperview()
+			make.leading.equalToSuperview()
+			make.trailing.equalToSuperview()
+			make.bottom.equalToSuperview()
+		}
+	}
+}
+
+// MARK: - UITableViewDelegate Implementation
+extension DevExamViewController: UITableViewDelegate {
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		//
+	}
+}
+
+// MARK: - UITableViewDataSource Implementation
+extension DevExamViewController: UITableViewDataSource {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		newsData.count
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = DevExamTableViewCell()
+
+		return UITableViewCell()
 	}
 }
 
