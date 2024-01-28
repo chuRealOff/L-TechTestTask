3d4f73074c45da6a59d84573fae42c3b56c07fca
Dmitry Churilov <98746743+chuRealOff@users.noreply.github.com>
2024-01-28 13:41:43 +0300

    tech: create a type that handles network errors

---
+++ L-TechTestTask/Supporting/NetworkError.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkError.swift
+//  L-TechTestTask
+//
+//  Created by CHURILOV DMITRIY on 28.01.2024.
+//
+
+import Foundation
+
+/// Кастомный тип данных для обработки ошибок, связанных с сетевым запросом.
+enum NetworkError: String, Error {
+	case unableToComplete = "Unable to complete your request. Please check your internet connection."
+	case invalidResponse = "Invalid response from the server. Please try again."
+	case invalidData = "The data received from the server was invalid. Please try again."
+}
