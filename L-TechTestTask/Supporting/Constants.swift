3b894c04f524c8ca11b4c705de708cb3151fcc41
Dmitry Churilov <98746743+chuRealOff@users.noreply.github.com>
2024-01-28 13:17:45 +0300

    add up the global state tableView rowHeight value

--- L-TechTestTask/Supporting/Constants.swift
+++ L-TechTestTask/Supporting/Constants.swift
@@ -5,7 +5,10 @@
 //  Created by CHURILOV DMITRIY on 11.12.2023.
 //
 
+import Foundation
+
 enum Constants {
 	static let jsonURLString = "http://dev-exam.l-tech.ru/api/v1/posts"
 	static let baseURLString = "https://dev-exam.l-tech.ru"
+	static let mainSceneEstimatedRowHeight: CGFloat = 100
 }
