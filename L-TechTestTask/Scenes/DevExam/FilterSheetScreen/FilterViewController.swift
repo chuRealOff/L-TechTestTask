cc3b49f45c0d92c18e67c854a8dbcf92b029c221
Dmitry Churilov <98746743+chuRealOff@users.noreply.github.com>
2024-01-28 13:15:03 +0300

    tech: perform FilterViewController layout work

---
+++ L-TechTestTask/Scenes/DevExam/FilterSheetScreen/FilterViewController.swift
@@ -0,0 +1,49 @@
+//
+//  FilterViewController.swift
+//  L-TechTestTask
+//
+//  Created by CHURILOV DMITRIY on 31.12.2023.
+//
+
+import UIKit
+
+/// Контроллер, отвечающий за сортировку содержимого экрана главного контроллера.
+class FilterViewController: UIViewController {
+	lazy private var tableView: UITableView = makeTableView()
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+	}
+
+	// MARK: - Private Methods
+	private func makeTableView() -> UITableView {
+		let tableView = UITableView()
+		tableView.register(DevExamTableViewCell.self, forCellReuseIdentifier: DevExamTableViewCell.identifier) // исправить
+		tableView.rowHeight = UITableView.automaticDimension
+		tableView.allowsSelection = true
+		tableView.delegate = self
+		tableView.dataSource = self
+
+		return tableView
+	}
+
+}
+
+// MARK: - UITableViewDelegate Implementation
+extension FilterViewController: UITableViewDelegate {
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		//
+	}
+}
+
+// MARK: - UITableViewDataSource Implementation
+extension FilterViewController: UITableViewDataSource {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		1
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		return UITableViewCell()
+	}
+}
