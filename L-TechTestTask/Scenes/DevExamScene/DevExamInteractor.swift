0f6a5b3f8f8fd650381ae44e28c2b6658d86dd23
Dmitry Churilov <98746743+chuRealOff@users.noreply.github.com>
2024-02-02 22:17:04 +0300

    tech: add up a coordinator to init method

--- L-TechTestTask/Scenes/DevExamScene/DevExamInteractor.swift
+++ L-TechTestTask/Scenes/DevExamScene/DevExamInteractor.swift
@@ -21,33 +21,34 @@ final class DevExamInteractor {
 	// MARK: - Dependencies
 	private let presenter: IDevExamPresenter
 	private let worker: IDevExamWorker
+	private let coordinator: IDevExamCoordinator
 
 	// MARK: - Initializer
-	init(presenter: IDevExamPresenter, worker: IDevExamWorker) {
+	init(presenter: IDevExamPresenter, worker: IDevExamWorker, coordinator: IDevExamCoordinator) {
 		self.presenter = presenter
 		self.worker = worker
+		self.coordinator = coordinator
 	}
 }
 
 // MARK: - IDevExamInteractor Implementation
 extension DevExamInteractor: IDevExamInteractor {
 	func viewIsReady() {
-		worker.fetchNetworkData(
-			from: Constants.jsonURLString) { [ weak self] result in
-				switch result {
-				case let .success((news, imageViews)):
-					self?.presenter.present(with: news, and: imageViews)
-				case .failure(let error):
-					if let alertController = self?.worker.createAlertController() {
-						DispatchQueue.main.async {
-							self?.presenter.presentErrorAlert(alertController, withMessage: error.rawValue)
-						}
+		worker.fetchNetworkData(from: Constants.jsonURLString) { [ weak self] result in
+			switch result {
+			case let .success((news, imageViews)):
+				self?.presenter.present(with: news, and: imageViews)
+			case .failure(let error):
+				if let alertController = self?.worker.createAlertController() {
+					DispatchQueue.main.async {
+						self?.presenter.presentErrorAlert(alertController, withMessage: error.rawValue)
 					}
 				}
 			}
+		}
 	}
 
 	func rowDidSelect(atIndexPath indexPath: IndexPath) {
-
+		coordinator.showDevExamDetailSceneFlow()
 	}
 }
