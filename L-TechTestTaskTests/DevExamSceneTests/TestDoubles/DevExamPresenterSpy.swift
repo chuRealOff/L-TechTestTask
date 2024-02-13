66f89c0625eb533b8c994d07c4018b828225ccf4
Dmitry Churilov <98746743+chuRealOff@users.noreply.github.com>
2024-02-13 13:11:04 +0300

    test: create DevExamPresenterSpy

---
+++ L-TechTestTaskTests/DevExamSceneTests/TestDoubles/DevExamPresenterSpy.swift
@@ -0,0 +1,22 @@
+//
+//  DevExamPresenterSpy.swift
+//  L-TechTestTaskTests
+//
+//  Created by CHURILOV DMITRIY on 13.02.2024.
+//
+
+import Foundation
+import UIKit
+@testable import L_TechTestTask
+
+final class DevExamPresenterSpy: IDevExamPresenter {
+	// MARK: Internal Properties
+	private(set) var presentWasCalled = false
+
+	// MARK: Internal Methods
+	func present(with newsData: [L_TechTestTask.DTO.NewsRawModel], and pictures: [UIImageView]) {
+		presentWasCalled = true
+	}
+	
+
+}
