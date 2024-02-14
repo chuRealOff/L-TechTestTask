9161e9533662e761d87b2bc5c918614d807a0cff
Dmitry Churilov <98746743+chuRealOff@users.noreply.github.com>
2024-02-14 11:43:26 +0300

    tech: create fake network loading protocol

---
+++ L-TechTestTaskTests/DevExamSceneTests/Mockable.swift
@@ -0,0 +1,36 @@
+//
+//  Mockable.swift
+//  L-TechTestTaskTests
+//
+//  Created by CHURILOV DMITRIY on 14.02.2024.
+//
+
+import Foundation
+
+/// Имитирует загрузку данных из сети, работая с моковыми данными.
+protocol Mockable: AnyObject {
+	var bundle: Bundle { get }
+	func loadData<T: Decodable>(fileName: String, type: T.Type) -> [T]
+}
+
+extension Mockable {
+	var bundle: Bundle {
+		return Bundle(for: type(of: self))
+	}
+
+	func loadData<T: Decodable>(fileName: String, type: T.Type) -> [T] {
+		guard let path = bundle.url(forResource: fileName, withExtension: "json") else {
+			fatalError("Failed to load JSON file.")
+		}
+
+		do {
+			let data = try Data(contentsOf: path)
+			let decodedObject = try JSONDecoder().decode([T].self, from: data)
+
+			return decodedObject
+		} catch {
+			print("❌ \(error)")
+			fatalError("Failed to decode the JSON.")
+		}
+	}
+}
